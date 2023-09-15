703a1bf1f00a5c011d2b7ea70f035c30f1957706
Dan Yunin <DaniilUnin2002@yandex.ru>
2023-09-15 12:47:39 +0300

    ready task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
+        this.currentResult = Math.round((max + min) / 2) ;
     }
 
     guess() {
-
+        return this.currentResult;
     }
 
     lower() {
-
+        this.maxNumber = this.currentResult;
+        this.currentResult = Math.round((this.maxNumber + this.minNumber) / 2) ;
     }
 
     greater() {
-
+        this.minNumber = this.currentResult;
+        this.currentResult = Math.round((this.maxNumber + this.minNumber) / 2);
     }
 }
 
